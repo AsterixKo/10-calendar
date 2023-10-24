c85023283e34015db00eb23c3d38c73d4bd71a26
Javier Moneo <jvr.moneo@gmail.com>
2023-10-24 14:10:50 +0200

    Preparar la creación de un nuevo evento

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -2,7 +2,7 @@ import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { addHours } from "date-fns";
 
-import { CalendarEvent, CalendarModal, Navbar } from "../";
+import { CalendarEvent, CalendarModal, FabAddNew, Navbar } from "../";
 import { localizer, getMessagesES } from "../../helpers";
 import { useState } from "react";
 import { useCalendarStore, useUiStore } from "../../hooks";
@@ -62,6 +62,7 @@ export const CalendarPage = () => {
         onView={onViewChange}
       />
       <CalendarModal />
+      <FabAddNew />
     </>
   );
 };
