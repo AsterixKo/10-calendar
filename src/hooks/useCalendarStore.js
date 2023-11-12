732887aadd06d45e746b6fc105be04fe9ab43163
Javier Moneo <jvr.moneo@gmail.com>
2023-11-12 18:02:03 +0100

    Actualizar el evento

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -8,6 +8,7 @@ import {
 } from "../store";
 import { calendarApi } from "../api";
 import { convertEventsToDateEvents } from "../helpers";
+import Swal from "sweetalert2";
 
 export const useCalendarStore = () => {
   const dispatch = useDispatch();
@@ -19,17 +20,25 @@ export const useCalendarStore = () => {
   };
 
   const startSavingEvent = async (calendarEvent) => {
-    // TODO: llegar al backend
+    try {
+      if (calendarEvent.id) {
+        // Actualizando
+        const { data } = await calendarApi.put(
+          `/events/${calendarEvent.id}`,
+          calendarEvent
+        );
+        // console.log(data);
 
-    // Todoo va bien
-    if (calendarEvent._id) {
-      // Actualizando
-      dispatch(onUpdateEvent({ ...calendarEvent }));
-    } else {
+        dispatch(onUpdateEvent({ ...calendarEvent, user }));
+        return;
+      }
       // Creando
       const { data } = await calendarApi.post("/events", calendarEvent);
       // console.log({ data });
       dispatch(onAddNewEvent({ ...calendarEvent, _id: data.evento.id, user }));
+    } catch (error) {
+      console.log(error);
+      Swal.fire("Error al guardar", error.response.data.msg, "error");
     }
   };
 
